a3221fc1e2337c536ed0946489018ded77b38543
Mikail Korkmaz <mikail-korkmaz@web.de>
2024-06-26 16:43:45 +0200

    initialized StatefulWidget's, implemented subfiles for future rendering content of multiple pages

--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz,{super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -9,7 +11,18 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/images/quiz-logo.png', width: 300),
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          /* Opacity(
+            opacity: 0.6,
+            child: Image.asset(
+              'assets/images/quiz-logo.png',
+               width: 300,
+               ),
+          ), !!Opacity is extremely resource intensive behind the scene!!*/
           const SizedBox(height: 80),
           const Text(
             'Learn Flutter the fun way!',
@@ -20,7 +33,9 @@ class StartScreen extends StatelessWidget {
           ),
           const SizedBox(height: 30),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: () {
+              startQuiz();
+            },
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
