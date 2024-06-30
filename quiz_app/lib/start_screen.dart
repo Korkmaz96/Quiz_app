16be320cacd116a941b7712843e0125be62fbf66
Mikail Korkmaz <mikail-korkmaz@web.de>
2024-06-30 23:03:57 +0200

    added more conditions for using the state to set up the active screen

--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen(this.startQuiz,{super.key});
+  const StartScreen(this.startQuiz, {super.key});
 
   final void Function() startQuiz;
 
@@ -34,9 +34,7 @@ class StartScreen extends StatelessWidget {
           ),
           const SizedBox(height: 30),
           OutlinedButton.icon(
-            onPressed: () {
-              startQuiz();
-            },
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
